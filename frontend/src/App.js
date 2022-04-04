a8b3841c3a0c05120b957a42496075de32a4b7b1
emna19 <benromdhaneamna19@gmail.com>
2022-04-04 15:12:49 +0100

    test back with front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api")
+      .then(response => response.json())
+      .then( data => { setBackendData(data)})
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => 
+        <p key={i}> {user} </p>)
+      )}
+    </div>
+  )
+}
+
+export default App;
+
+// eslint-disable-next-line no-lone-blocks
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +39,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
