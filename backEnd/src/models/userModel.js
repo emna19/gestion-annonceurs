622d8aca0c2cbb49f0d51abb3553361b0a11d2e0
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-04-06 16:03:14 +0200

    commenting

--- backEnd/src/models/userModel.js
+++ backEnd/src/models/userModel.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose')
 
-//creating User
+//creating User schema
 const userSchema = new mongoose.Schema({
     name : String , 
     age: Number
 
 });
-
+// creating user model 
  const User =  new mongoose.model('User',userSchema)
-//  module.exports = User
-
+//checking connectivity to db with a model 
 const user = async()=>{
   const result = await User.insertMany([{name: 'beep' , age : 30 },{name: 'boop' , age : 30 }])
   console.log(result);
@@ -18,6 +17,8 @@ const user = async()=>{
 
 
 user();
+//  module.exports = User
+
 
 
 
