8ff9c8538306bf04a0e9200afe57c7451e9b2922
emna19 <benromdhaneamna19@gmail.com>
2022-04-06 13:44:21 +0100

    installing body parser and creating table user with post api

---
+++ backEnd/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    organistaion: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    adress: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    codePostal: {
+        type: Number,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true
+    }
+},{timestamps: true});//timestamp for updated at and created at
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,8 +1,43 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const User = require("./models/user")
+const bodyParser = require("body-parser")
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(bodyParser.json());
 
 app.get('/api', function (req, res) {
   res.json({"users": ["userOne", "userTwo", "userThree", "userFour"] })
 })
+
+app.post('/add_user', async(req,res) => {
+  try {
+      let new_user = new User({
+        name: req.body.name,
+        organistaion: req.body.organistaion,
+        email: req.body.email,
+        password: req.body.password,
+        adress: req.body.adress,
+        phone: req.body.phone,
+        country: req.body.country,
+        city: req.body.city,
+        codePostal: req.body.codePostal,
+        isAdmin: req.body.isAdmin
+      });
+      await new_user.save();
+      res.send('save effectué par succés!');
+  }
+  catch (err) {
+      console.log(err);
+  }
+});
+
+mongoose.connect("mongodb://localhost:27017/gestion-annonceurs", (err, done) => {
+    if (err) console.log(err);
+    if (done) console.log('base de données connecté avec succes !');
+});
 
 app.listen(5000, () => console.log("Server started on port 5000"))
