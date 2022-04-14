3875f7a38a273bdabb138635d675230a72148719
emna19 <benromdhaneamna19@gmail.com>
2022-04-14 13:49:53 +0100

    setting errors

--- frontend/src/components/signUp/signUp.js
+++ frontend/src/components/signUp/signUp.js
@@ -6,24 +6,23 @@ export default function SignUp() {
     const url = "http://localhost:5000/users"
     const [user, setUser] = useState(
         {
-            name: 'Name',
-            photo: 'Photo',
-            email: 'Email',
-            password: 'Password',
-            organistaion: 'Organisation',
-            phone: 'Phone',
-            country: 'Country',
-            city: 'City',
-            codePostal: 'Postcode',
-            taxID: 'Tax ID',
-            adress: 'Adresse',
+            name: '',
+            photo: '',
+            email: '',
+            password: '',
+            organistaion: '',
+            phone: '',
+            country: '',
+            city: '',
+            codePostal: '',
+            taxID: '',
+            adress: '',
     });
 
     function handle(e) {
         const newUser = {...user}
         newUser[e.target.id] = e.target.value
         setUser(newUser)
-        console.log(user)
     }
 
     function submit(e) {
@@ -43,7 +42,7 @@ export default function SignUp() {
             adress: user.adress,
             isAdmin: true
         })
-        .then(res => {console.log(res.data)})
+        .then(res => {window.location.href = '/login'})
     }
         
 
@@ -55,47 +54,47 @@ export default function SignUp() {
                     <form className="row g-2" onSubmit={submit}>
                         <div className="input-elements row g-2">
                             <div className="col-md-7">
-                                <input type="text" className="form-control" onChange={handle} id="name" value={user.name} required/>
+                                <input type="text" className="form-control" onChange={handle} id="name" value={user.name} placeholder= "Name" required/>
                             </div>
                             <div className="col-md-5">
-                                <input type="text" className="form-control" onChange={handle} id="photo" value={user.photo} required/>
+                                <input type="text" className="form-control" onChange={handle} id="photo" value={user.photo} placeholder= "Photo" required/>
                             </div>
                         </div>
                         <div className="input-elements row g-2">
                             <div className="col">
                                 <div className="Email input-group has-validation">
                                     <span className="input-group-text" id="inputGroupPrepend">@</span>
-                                    <input type="text" className="Email-text form-control" onChange={handle} id="email" value={user.email} aria-describedby="inputGroupPrepend" required/>
+                                    <input type="email" className="Email-text form-control" onChange={handle} id="email" value={user.email} placeholder= "Email" aria-describedby="inputGroupPrepend" required/>
                                 </div>
                             </div>
                         </div>
                         <div className="input-elements row g-2">
                             <div className="col-md-5">
-                                <input type="text" className="form-control" onChange={handle} id="organistaion" value={user.organistaion} required/>
+                                <input type="text" className="form-control" onChange={handle} id="organistaion" value={user.organistaion} placeholder= "Organisation" required/>
                             </div>
                             <div className="col-md-7">
-                                <input type="text" className="form-control" onChange={handle} id="password" value={user.password} required/>
+                                <input type="password" className="form-control" onChange={handle} id="password" value={user.password} placeholder= "Password" required/>
                             </div>
                         </div>
                         <div className="input-elements row g-2">
                             <div className="col-md-7">
-                                <input type="text" className="form-control" onChange={handle} id="phone" value={user.phone} required/>
+                                <input type="text" className="form-control" onChange={handle} id="phone" value={user.phone} placeholder= "Phone" required/>
                             </div>
                             <div className="col-md-5">
-                                <input type="text" className="form-control" onChange={handle} id="taxID" value={user.taxID} required/>
+                                <input type="text" className="form-control" onChange={handle} id="taxID" value={user.taxID} placeholder= "Tax ID" required/>
                             </div>
                         </div>
                         <div className="input-elements row g-2">
                             <div className="col-md-7">
-                                <input type="text" className="form-control" onChange={handle} id="adress" value={user.adress} required/>
+                                <input type="text" className="form-control" onChange={handle} id="adress" value={user.adress} placeholder= "Adresse" required/>
                             </div>
                             <div className="col-md-5">
-                                <input type="text" className="form-control" onChange={handle} id="codePostal" value={user.codePostal} required/>
+                                <input type="text" className="form-control" onChange={handle} id="codePostal" value={user.codePostal} placeholder= "Postcode" required/>
                             </div>
                         </div>
                         <div className="input-elements row g-2">
                             <div className="col-md-4">
-                                <input type="text" className="form-control" onChange={handle} id="country" value={user.country} required/>
+                                <input type="text" className="form-control" onChange={handle} id="country" value={user.country} placeholder= "Country" required/>
                             </div>
                             <div className="col-md-4">
                                 <select className="form-select" id="validationCustom04" defaultValue="" required>
@@ -104,8 +103,8 @@ export default function SignUp() {
                                 </select>
                             </div>
                             <div className="col-md-4">
-                                <select className="form-select" onChange={handle} id="city" value={user.city} required>
-                                    <option disabled>City</option>
+                                <select className="form-select" onChange={handle} id="city" value={user.city} placeholder="" required>
+                                    <option value="" disabled >City</option>
                                     <option>...</option>
                                 </select>
                             </div>
