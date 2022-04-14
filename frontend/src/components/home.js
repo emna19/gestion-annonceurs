a72691a35028f0d9cf68508f8a7eb8a9337ce57d
emna19 <benromdhaneamna19@gmail.com>
2022-04-14 02:55:45 +0100

    nav and main

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -3,7 +3,22 @@ import './home.css'
 export default function Home() {
     return(
         <div>
-            aaaa
+            <nav className="navbar">
+                <span className="navbar-brand mb-0 h1">Artify Ads</span>
+            </nav>
+            <main>
+                <div className="statics container">
+                    <h5>Statistics</h5>
+                </div>
+                <div className="announcements container">
+                    <h5>Announcements</h5>
+                    <button type="button">+ Add</button>
+                </div>
+                <div className="audiences container">
+                    <h5>Audiences</h5>
+                    <button type="button">+ Add</button>
+                </div>
+            </main>
         </div>
     )
 }
