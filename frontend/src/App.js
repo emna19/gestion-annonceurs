75903fe15172beae70ad93c225662ac5fa10d846
emna19 <benromdhaneamna19@gmail.com>
2022-04-14 02:01:33 +0100

    add home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home'
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/api")
-      .then(response => response.json())
-      .then( data => { setBackendData(data)})
-  }, [])
 
   return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => 
-        <p key={i}> {user} </p>)
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
