774574f986404309804a141feca1580ec6e6d548
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-04-14 18:04:50 +0200

    Update home.js

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,22 +1,9 @@
-import './home.css'
+import React from 'react'
 
-export default function Home() {
-    return(
-        <div>
-            
-            <main>
-                <h5>Statistics</h5>
-                <div className="statics container">
-                </div>
-                <h5>Announcements</h5>
-                <button type="button">+ Add</button>
-                <div className="announcements container">   
-                </div>
-                <h5>Audiences</h5>
-                <button type="button">+ Add</button>
-                <div className="audiences container"> 
-                </div>
-            </main>
-        </div>
-    )
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
 }
+
+export default Home
