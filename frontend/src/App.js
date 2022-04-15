13019ee4c767868b6b7a9376db3599708458972b
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-04-15 05:52:25 +0200

    adding profile route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import SignUp from "./components/signUp/signUp"
 import Login from "./components/login/Login";
 import Error from "./components/ErrorPage/Error";
 import ResetPassword from "./components/ResetPassword";
-import Profil from './components/Profil/Profil';
+import Profile from './components/Profile/Profile';
 // import Home from './components/Home/Home';
 // import Navbar from './components/navbar/navbar'
 
@@ -21,7 +21,7 @@ function App() {
         <Route  exact path="/login" element={ <Login/>} />
 
         {/* <Route  exact path="/Home" element={ <div><Navbar /> <Home /></div>} /> */}
-        <Route exact path="/Profil" element={ <Profil/>}/>
+        <Route exact path="/Profil" element={ <Profile/>}/>
         <Route  exact path="/RestPassword" element={ <ResetPassword/>} />
         <Route exact path="*" element={<Error/>} />
        
