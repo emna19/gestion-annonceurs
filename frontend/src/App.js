8a621c23e295471deded14fc917fdd4f4fbcec85
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-04-16 06:56:27 +0200

    setting profile frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,7 @@ import Login from "./components/login/Login";
 import Error from "./components/ErrorPage/Error";
 import ResetPassword from "./components/ResetPassword";
 import Profile from './components/Profile/Profile';
-
-import Home from './components/home/home';
+// import Home from './components/Home/Home';
 import Navbar from './components/navbar/navbar'
 
 
@@ -22,7 +21,7 @@ function App() {
         <Route  exact path="/login" element={ <Login/>} />
 
         {/* <Route  exact path="/Home" element={ <div><Navbar /> <Home /></div>} /> */}
-        <Route exact path="/Profil" element={ <Profile/>}/>
+        <Route exact path="/Profile" element={ <div> <Navbar /><br/> <Profile/></div> }/>
         <Route  exact path="/RestPassword" element={ <ResetPassword/>} />
         <Route exact path="*" element={<Error/>} />
        
--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from "react";
+
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="row">
+      <div className="col-xl-4">
+        {/* <!-- Profile picture card--> */}
+        <div className="card mb-4 mb-xl-0">
+          <div className="card-header">Profile Picture</div>
+          <div className="card-body text-center">
+            {/* <!-- Profile picture image--> */}
+            <img
+              className="img-account-profile rounded-circle mb-2"
+              src="http://bootdey.com/img/Content/avatar/avatar1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-8">
+        {/* <!-- Account details card--> */}
+          <div className="card mb-4">
+            <div className="card-header">Account Details</div>
+            <div className="card-body">
+              {/* <!-- Form Group (username)--> */}
+              <div className="mb-3">
+                <label className="small mb-1" for="inputUsername">
+                  Username{" "}
+                </label>
+              </div>
+              {/* <!-- Form Row--> */}
+              <div className="row gx-3 mb-3" >
+                {/* <!-- Form Group (first name)--> */}
+                <div className="col-md-6">
+                  <label className="small mb-1" for="inputFirstName">
+                    First name
+                  </label>
+                </div>
+                {/* <!-- Form Group (last name)--> */}
+                <div className="col-md-6">
+                  <label className="small mb-1" for="inputLastName">
+                    Last name
+                  </label>
+                </div>
+              </div>
+              {/* <!-- Form Row        --> */}
+              <div className="row gx-3 mb-3">
+                {/* <!-- Form Group (organization name)--> */}
+                <div className="col-md-6">
+                  <label className="small mb-1" for="inputOrgName">
+                    Organization name
+                  </label>
+                </div>
+                {/* <!-- Form Group (location)--> */}
+                <div className="col-md-6">
+                  <label className="small mb-1" for="inputLocation">
+                    Location
+                  </label>
+                </div>
+              </div>
+              {/* <!-- Form Group (email address)--> */}
+              <div className="mb-3">
+                <label className="small mb-1" for="inputEmailAddress">
+                  Email address
+                </label>
+              </div>
+              {/* <!-- Form Row--> */}
+              <div className="row gx-3 mb-3">
+                {/* <!-- Form Group (phone number)--> */}
+                <div className="col-md-6">
+                  <label className="small mb-1" for="inputPhone">
+                    Phone number
+                  </label>
+                </div>
+                {/* <!-- Form Group (birthday)--> */}
+                <div className="col-md-6">
+                  <label className="small mb-1" for="inputBirthday">
+                    Birthday
+                  </label>
+                </div>
+              </div>
+              {/* <!-- Save changes button--> */}
+              <button className="btn btn-primary" type="button">
+                Change details
+              </button>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
