998f526671ebf14d437eeea7646f0544b33deb79
emna19 <benromdhaneamna19@gmail.com>
2022-04-20 21:50:49 +0100

    added the validation

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -86,10 +86,17 @@ export default function Home() {
                       <span className="col-auto card-text">Source url:</span>
                       <div className='col-7 text-start keywords'>{ item.sourceUrl }</div>
                     </div>
-                    <div className="row mb-3">
+                    <div className="row mb-4">
                       <span className="col-auto card-text">Type:</span>
                       <div className='col-8 text-start keywords'>{ item.type }</div>
                     </div>
+                    {item.isValid && <div className='row mb-2 validation'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" className="col-auto bi bi-x-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                      </svg>
+                      <div className='col-auto'>Waiting for validation</div>
+                    </div>}
                   </div>
                 </div>
                 ))}
