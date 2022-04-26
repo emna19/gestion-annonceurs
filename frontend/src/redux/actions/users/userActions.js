daf83b7e7c739101a3dfe8112ba60ce10f906b0f
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-04-27 00:15:02 +0200

    creating actions and reducers

--- frontend/src/redux/actions/users/userActions.js
+++ frontend/src/redux/actions/users/userActions.js
@@ -1,22 +1,36 @@
 import axios from "axios";
-import { USER_REGISTER_REQUEST } from "../actionTypes";
+import {
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from "../actionTypes";
 
+/// create user action
 
-/// create user action 
+const createUserAction = (userData) => {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: USER_REGISTER_REQUEST,
+      });
 
-const createUserAction = userData =>{
-    return async dispatch =>{
-        dispatch({
-            type:USER_REGISTER_REQUEST,
-        });
-        
-        const config = {
-        'Content-Type' : 'application/json',
-        };
+      const config = {
+        "Content-Type": "application/json",
+      };
 
-        const res = await axios.post('http://localhost:5000/users',  )
+      const { data } = await axios.post("/users", userData, config);
 
-    };
+      dispatch({    //user register succeess
+        type: USER_REGISTER_SUCCESS, 
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({     //user register fail
+        type: USER_REGISTER_FAIL, 
+        payload: error.response && error.response.data.message,
+      });
+    }
+  };
+};
 
-
-}
+export {createUserAction} ; 
