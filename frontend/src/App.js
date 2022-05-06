36f8eeee2f665a484803e944754793475ea99837
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-05-07 00:39:21 +0200

    removing forget password in login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,13 @@ import UpdateProfile from './components/updateProfile/updateProfile';
 import SignUp from "./components/signUp/signUp"
 import Login from "./components/login/Login";
 import Error from "./components/ErrorPage/Error";
-import ResetPassword from "./components/ResetPassword";
 import Profile from './components/Profile/Profile';
 import Home from './components/home/home'
 import Navbar from './components/navbar/navbar'
 import Audience from './components/audiences/Audience';
 import CreateAnnonce from './components/annonce/Annonce'
-import AudView from './components/audiences/AudView'
-import AnnView from './components/annonce/AnnView'
+// import AudView from './components/audiences/AudView'
+// import AnnView from './components/annonce/AnnView'
 
 
 
@@ -30,7 +29,6 @@ function App() {
         <Route  exact path="/home/annonce/create" element={ <CreateAnnonce/>} />
 
         <Route  exact path="/home" element={ <div><Navbar /> <Home /></div>} />
-        <Route  exact path="/RestPassword" element={ <ResetPassword/>} />
         <Route exact path="*" element={<Error/>} />
        
       </Routes>
