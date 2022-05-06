26e3044eac0b78be34948ca9a8386aa13652ec2e
emna19 <benromdhaneamna19@gmail.com>
2022-05-06 03:17:14 +0100

    Revert "added the store"
    This reverts commit 6d1c6e048b3e837cab4fbd77e26eeac5a39eda56.

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -61,7 +61,28 @@ export default function Home() {
       zIndex: viewClicked ? -1 : null,
       height: viewClicked ? "660px" : null,
       backgroundColor: viewClicked ? "#00000094" : null,
+    },
+
+    buttonDeleteAudience: {
+      color: isCheckedAudience.length ===0 ? "grey": null,
+      border: isCheckedAudience.length ===0 ? "1px solid #ffffff00" : null, 
+      cursor: isCheckedAudience.length ===0 ? "not-allowed" : "pointer"
+    },
+
+    svgXAudience: {
+      fill: isCheckedAudience.length ===0 ? "grey" : "#F17B22"
+    },
+
+    buttonDeleteAnnonce: {
+      color: isCheckedAnnonce.length ===0 ? "grey": null,
+      border: isCheckedAnnonce.length ===0 ? "1px solid #ffffff00" : null, 
+      cursor: isCheckedAnnonce.length ===0 ? "not-allowed" : "pointer"
+    },
+
+    svgXAnnonce: {
+      fill: isCheckedAnnonce.length ===0 ? "grey" : "#F17B22"
     }
+
   }
 
   function addAnnonce(e) {
@@ -121,7 +142,6 @@ export default function Home() {
   }
 
   let labels=[]
-  let age=[]
   let name=[]
   
 
@@ -168,10 +188,14 @@ export default function Home() {
           </div>
           <div className="home-container-head">
             <h5 className="home-container-title">Advertisements</h5>
-            <div className='row mb-2 justify-content-between'>
+            <div className='row mb-2 justify-content-between align-items-center'>
+              <div className="col-auto form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" onChange={handleSelectAllAnnonce} checked={isCheckedAllAnnonce} />
+                  <label className="form-check-label" htmlFor="flexCheckDefault">Select All</label>
+              </div>
               <div className='col-auto'>
-                <button className="home-container-Delete" type="button" onClick={deleteAnnonce}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#F17B22" className="bi bi-x" viewBox="0 0 16 16">
+                <button style={styles.buttonDeleteAnnonce} className="home-container-Delete" type="button" onClick={deleteAnnonce}>
+                  <svg style={styles.svgXAnnonce} xmlns="http://www.w3.org/2000/svg" width="25" height="25" className="bi bi-x" viewBox="0 0 16 16">
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                   </svg>
                   Delete Selected
@@ -186,10 +210,6 @@ export default function Home() {
 
           <div id="carouselExampleControlsNoTouching" className="carousel slide" data-bs-touch="false" data-bs-interval="false">  
             <div className="row carousel-inner audiences announcements login-container">
-              <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" onChange={handleSelectAllAnnonce} checked={isCheckedAllAnnonce} style={{float: "right"}}/>
-                  <label className="form-check-label" htmlFor="flexCheckDefault"></label>
-              </div>
               {annonce.map((item, index) =>(
                   <div key={index} className="card text-center">
                   <div className="card-body">
@@ -235,10 +255,14 @@ export default function Home() {
 
           <div className="home-container-head">
             <h5 className="home-container-title">Audiences</h5>
-            <div className='row mb-2 justify-content-between'>
+            <div className='row mb-2 justify-content-between align-items-center'>
+            <div className="col-auto form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" onChange={handleSelectAllAudience} checked={isCheckedAllAudience}/>
+                  <label className="form-check-label" htmlFor="flexCheckDefault">Select All</label>
+              </div>
               <div className='col-auto'>
-                <button className="home-container-Delete" type="button" onClick={deleteAudience}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#F17B22" className="bi bi-x" viewBox="0 0 16 16">
+                <button className="home-container-Delete" style={styles.buttonDeleteAudience} type="button" onClick={deleteAudience}>
+                  <svg style={styles.svgXAudience} xmlns="http://www.w3.org/2000/svg" width="25" height="25" className="bi bi-x" viewBox="0 0 16 16">
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                   </svg>
                   Delete Selected
@@ -251,10 +275,7 @@ export default function Home() {
           </div>                
           <div id="carouselExampleControlsNoTouching" className="carousel slide" data-bs-touch="false" data-bs-interval="false">
             <div className="row carousel-inner audiences login-container">
-              <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" onChange={handleSelectAllAudience} checked={isCheckedAllAudience} style={{float: "right"}}/>
-                  <label className="form-check-label" htmlFor="flexCheckDefault"></label>
-              </div>
+              
             {audience.map((item, index) =>(
                 
                 <div key={index} className="card text-center">
