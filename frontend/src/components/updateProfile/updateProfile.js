00f09cf4113fa5b85614c667f0c351230df58763
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2022-06-05 02:39:13 +0200

    admin interface profile and update

--- frontend/src/components/updateProfile/updateProfile.js
+++ frontend/src/components/updateProfile/updateProfile.js
@@ -42,7 +42,7 @@ const UpdateProfile = () => {
   const [codePostal, setcodePostal] = useState(user.codePostal);
   const [taxID, settaxID] = useState(user.taxID);
   const [adress, setadress] = useState(user.adress);
-  const [state] = useState(user.state);
+  const [isAdmin] = useState(user.isAdmin);
   //  const [photo, setphoto] = useState(user.photo)
 
   //dispatch  update action
@@ -66,7 +66,7 @@ const UpdateProfile = () => {
         codePostal,
         taxID,
         adress,
-        state
+      
         // photo
       )
     );
@@ -76,6 +76,100 @@ const UpdateProfile = () => {
     navigate("/Profile");
   };
 
+  if (isAdmin)
+  /// user is admin 
+  return (
+    <form onSubmit={SubmitHandler}>
+      <div className="update-profile">
+        <div className="update-profile-picture-frame">
+          <img
+            className="update-profile-image"
+            src={require("../../assets/avatar.png")}
+            alt=""
+          />
+          <div className="update-profile-name-frame">
+            <input
+              className="update-profile-Name  update-profile-style"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
+              type="text"
+              placeholder="Name"
+            />
+          </div>
+        </div>
+        <div className="update-profile-details-frame">
+          <div className="update-profile-account-details">Account Details</div>
+          <input
+            className="update-profile-account-email  update-profile-style"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            type="email"
+            placeholder=" Email "
+          />
+
+          <input
+            className="update-profile-account-password  update-profile-style"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+            type="password"
+            placeholder=" Password "
+          />
+
+          <input
+            className="update-profile-account-phone   update-profile-style"
+            value={phone}
+            onChange={(e) => setphone(e.target.value)}
+            type="number"
+            placeholder=" phone "
+          />
+
+
+          <input
+            className="update-profile-account-adress update-profile-style"
+            value={adress}
+            onChange={(e) => setadress(e.target.value)}
+            type="text"
+            placeholder="adress"
+          />
+
+          <input
+            className="update-profile-account-postcode update-profile-style"
+            value={codePostal}
+            onChange={(e) => setcodePostal(e.target.value)}
+            type="number"
+            placeholder="postcode"
+          />
+
+          <input
+            className="update-profile-account-country update-profile-style"
+            value={country}
+            onChange={(e) => setcountry(e.target.value)}
+            type="text"
+            placeholder="country"
+          />
+
+          <input
+            className="update-profile-account-city update-profile-style"
+            value={city}
+            onChange={(e) => setcity(e.target.value)}
+            type="text"
+            placeholder="city"
+          />
+
+          <button className="update-profile-button" type="submit">
+            Save Changes
+          </button>
+          {/* <button className="return-to-profile-button" type="submit">
+             <Link to="/Profile" className='save-changes' >
+            Return to profile
+            </Link> 
+          </button> */}
+        </div>
+      </div>
+    </form>
+  );
+
+  //// user is not admin
   return (
     <form onSubmit={SubmitHandler}>
       <div className="update-profile">
