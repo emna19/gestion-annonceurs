7c2c6e862ef9bb67081fe7dc698fa2ce28aa00f5
emna19 <62568833+emna19@users.noreply.github.com>
2022-06-06 02:58:14 +0100

    Revert "Edit home page and its components"

--- frontend/src/components/Admin/AnnViewAdmin.js
+++ frontend/src/components/Admin/AnnViewAdmin.js
@@ -40,19 +40,6 @@ export default function AnnViewAdmin(props) {
         }
     }
 
-
-    const state = {
-        reload: false
-      };
-    
-    const refreshPage = () => {
-        this.setState(
-          {reload: true},
-          () => (this.setState({reload: false}))
-        )
-    }
-    console.log("okk")
-
     function handle(e) {
         const newAnnonce = {...annonce}
         if (e.target.id === "startDate" || e.target.id === "endtDate" ) {newAnnonce[e.target.id] = new Date(e.target.value).toISOString()}
@@ -155,7 +142,7 @@ export default function AnnViewAdmin(props) {
                     <div className='text-center'>
                     <button id="isValid" type="button" className="home-container-Add"
                     style={{height: "38px"}}
-                    onClick={() =>  {return(validate(),window.location.href = '/Admin')}}>
+                    onClick={() =>  {return(validate())}}>
                             Validate
                     </button>
                     </div>
